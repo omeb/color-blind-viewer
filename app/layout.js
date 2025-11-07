a7951bb96b90e0199ff39125e40b98499f497dfc
omeb <omerb@wix.com>
2025-11-07 20:08:23 +0200

    Fix hydration error by adding suppressHydrationWarning to html element

--- app/layout.js
+++ app/layout.js
@@ -92,7 +92,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         {/* Apply theme immediately to prevent flash */}
         <script
